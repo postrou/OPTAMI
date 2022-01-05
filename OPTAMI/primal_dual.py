primal_dual: speeded up a bit estim_seq_subproblem, some minor fixes run_pd_experiment: some refactoring, fixed i incrementation in optimize, added lipschitz calculation function

OPTAMI/primal_dual.py
@@ -57,19 +57,18 @@ class PrimalDualAccelerated(Optimizer):
 
         # filling state
         state = self.state['default']
-        grad_phi_k = []
+        grad_phi_k = [(None for _ in params)]
         for param in params:
             param_copy = param.clone().detach()
             assert len(param_copy.shape) <= 2, "May be some troubles with tensor of higher order"
             # if len(param_copy.shape) == 2:
 
             # assert torch.all(param_copy == 0), 'Initial point should be all zeros!'
-            grad_phi_k.append(param_copy) # since we won't need \grad \phi(\lambda_0)
+            # grad_phi_k.append(param_copy) # since we won't need \grad \phi(\lambda_0)
         state['x_hat'] = []
-        state['grad_phi_k'] = [tuple(grad_phi_k)]
+        state['grad_phi_k'] = grad_phi_k
 
-        A = self._calculate_A(0, group)
-        state['A_k'] = [A]
+        state['A_k'] = [0.0]
 
     def step(self, closure=None):
         if closure is None:
@@ -84,7 +83,7 @@ class PrimalDualAccelerated(Optimizer):
         state = self.state['default']
         A_k = state['A_k']
         A = A_k[-1]
-        k = len(A_k) - 1    # because we keep A_next too
+        k = len(A_k) - 1    # because we increase k in the end of the step
 
         # step 3
         # print('Step 3: Computation of v_k...')
@@ -143,7 +142,8 @@ class PrimalDualAccelerated(Optimizer):
 
         p_fact = ttv.factorial(p_order)
         one_over_p = 1 / p_order
-        fst_factor = (p_fact / C) ** one_over_p
+        # fst_factor = (p_fact / C) ** one_over_p
+        fst_factor = p_fact / C
 
         results = [torch.empty_like(param) for param in params]
         for i, param in enumerate(params):
@@ -154,7 +154,10 @@ class PrimalDualAccelerated(Optimizer):
                 grad_phi = grad_phi_k[j][i]
                 grad_sum += (A - A_prev) * grad_phi
 
-            snd_factor = grad_sum / (grad_sum.norm() ** (1 - one_over_p))
+            # snd_factor = grad_sum / (grad_sum.norm() ** (1 - one_over_p))
+            grad_sum_norm = grad_sum.norm()
+            snd_factor = grad_sum / grad_sum_norm
+            fst_factor = (fst_factor * grad_sum_norm) ** one_over_p
             results[i] = -fst_factor * snd_factor
         return results
 

run_pd_experiment.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np
 from scipy.spatial.distance import cdist
 from mnist import MNIST
+from tqdm import trange
 
 from OPTAMI import *
 
@@ -30,35 +31,35 @@ def cartesian_product(*arrays):
     return arr.reshape(-1, la)
 
 
-def calculate_x(lamb, half_lamb_len, gamma, M_over_gamma):
+def calculate_x(lamb, half_lamb_len, gamma, M_matrix_over_gamma):
     psi = lamb[:half_lamb_len]
     eta = lamb[half_lamb_len:]
     psi_outer = torch.outer(psi, torch.ones(len(psi)))
     eta_outer = torch.outer(torch.ones(len(eta)), eta)
     lamb_factor_over_gamma = (psi_outer + eta_outer) / gamma
-    under_exp_vector = (lamb_factor_over_gamma - M_over_gamma).view(-1)
+    under_exp_vector = (lamb_factor_over_gamma - M_matrix_over_gamma).view(-1)
     return torch.softmax(under_exp_vector, dim=0)
 
 
-def phi(lamb, optimizer, half_lamb_len, gamma, M_over_gamma, b):
+def phi(lamb, optimizer, half_lamb_len, gamma, M_matrix_over_gamma, b):
     optimizer.zero_grad()
     psi = lamb[:half_lamb_len]
     eta = lamb[half_lamb_len:]
     psi_outer = torch.outer(psi, torch.ones(len(psi)))
     eta_outer = torch.outer(torch.ones(len(eta)), eta)
     lamb_factor_over_gamma = (psi_outer + eta_outer) / gamma
-    under_exp_vector = (lamb_factor_over_gamma - M_over_gamma).view(-1)
+    under_exp_vector = (lamb_factor_over_gamma - M_matrix_over_gamma).view(-1)
     return torch.logsumexp(under_exp_vector, dim=0) - lamb @ b
 
 
-def f(x, M, gamma):
+def f(x, M_matrix, gamma):
     x_copy = x.detach().clone()
     x_copy_under_log = x_copy.clone()
     x_copy_under_log[x_copy == 0.] = 1e-6
-    return (M.view(-1) * x_copy).sum() + gamma * (x_copy * np.log(x_copy_under_log)).sum()
+    return (M_matrix.view(-1) * x_copy).sum() + gamma * (x_copy * np.log(x_copy_under_log)).sum()
 
 
-def optimize(optimizer, closure, eps, M, gamma):
+def optimize(optimizer, closure, eps, M_matrix, gamma, max_steps=100):
     i = 1
     while True:
         optimizer.step(closure)
@@ -67,33 +68,48 @@ def optimize(optimizer, closure, eps, M, gamma):
             x_hat = optimizer.state['default']['x_hat'][0]
             lamb = optimizer.param_groups[0]['params'][0]
             phi_value = closure()
-            f_value = f(x_hat, M, gamma)
+            f_value = f(x_hat, M_matrix, gamma)
             criterion = abs(phi_value + f_value)
 
             print('\n'.join(
                 [
                     f'Step #{i}, criterion={criterion}, phi={phi_value}, f={f_value}',
-                    f'lambda.sum()={lamb.detach().sum()}',
-                    f'x_hat.sum()={x_hat.detach().sum()}'
+                    f'lambda={lamb.detach()}',
+                    f'x_hat={x_hat.detach()}'
                 ]), end='\n\n')
+
+            if criterion < eps or i == max_steps:
+                break
 
             i += 1
 
-            if criterion < eps:
-                break
+
+def calculate_lipschitz_constant(n, gamma, p_order=3):
+    A = torch.hstack([torch.eye(n)] * n)
+    vectors = torch.vstack(
+        [torch.hstack([
+            torch.zeros(1, n) if j != i else torch.ones(1, n) for j in range(n)
+        ]) for i in trange(n, desc='Building matrix A')]
+    )
+    A = torch.vstack((A, vectors))
+    A_A_T = A @ A.T
+    _, s, _ = torch.svd(A_A_T, compute_uv=False)
+    if p_order == 3:
+        return s.max() ** 2 * 15 / gamma ** 3
+    else:
+        raise NotImplementedError(f'Lipschitz constant calculation for p={p_order} is not implemented!')
 
 
-def run_experiment(M_p, gamma, eps, image_index=0):
+def run_experiment(M_p, gamma, eps, image_index=0, max_steps=100):
     images, labels = load_data()
-    l = len(images)
     n = len(images[0])
     m = int(np.sqrt(n))
 
-    M = np.arange(m)
-    M = cartesian_product(M, M)
-    M = cdist(M, M)
-    M /= np.max(M)
-    M = torch.tensor(M)
+    M_matrix = np.arange(m)
+    M_matrix = cartesian_product(M_matrix, M_matrix)
+    M_matrix = cdist(M_matrix, M_matrix)
+    M_matrix /= np.max(M_matrix)
+    M_matrix = torch.tensor(M_matrix)
 
     # experiments were done for
     p_list = [34860, 31226, 239, 37372, 17390]
@@ -106,24 +122,29 @@ def run_experiment(M_p, gamma, eps, image_index=0):
     p, q = mnist(epsp, p_list[image_index], q_list[image_index], images, n)
     b = torch.cat((torch.tensor(p), torch.tensor(q)))
 
-    # lamb = torch.zeros(n * 2, dtype=torch.double, requires_grad=True)
-    lamb = torch.tensor([1e-6] * (n * 2), dtype=torch.double, requires_grad=True)
+    lamb = torch.zeros(n * 2, dtype=torch.double, requires_grad=True)
+    # lamb = torch.tensor([1e-6] * (n * 2), dtype=torch.double, requires_grad=True)
     half_lamb_len = int(len(lamb) / 2)
-    M_over_gamma = M / gamma
+    M_matrix_over_gamma = M_matrix / gamma
 
     optimizer = PrimalDualAccelerated(
         [lamb],
         M_p=M_p,
         eps=0.01,
-        calculate_x_function=lambda lamb: calculate_x(lamb, half_lamb_len, gamma, M_over_gamma)
+        calculate_x_function=lambda lamb: calculate_x(lamb, half_lamb_len, gamma, M_matrix_over_gamma)
     )
-    closure = lambda: phi(lamb, optimizer, half_lamb_len, gamma, M_over_gamma, b)
-    optimize(optimizer, closure, eps, M, gamma)
+    closure = lambda: phi(lamb, optimizer, half_lamb_len, gamma, M_matrix_over_gamma, b)
+    optimize(optimizer, closure, eps, M_matrix, gamma, max_steps)
+    return optimizer
 
 
 if __name__ == '__main__':
     eps = 1e-3
-    M_p = 3 * 1e+8
-    gamma = eps / 3 / np.log(n)
+    n = 784
     image_index = 0
+    gamma = 0.9
+
+    print(f'Calculating Lipschitz constant...')
+    M_p = calculate_lipschitz_constant(n, gamma, p_order=3)
+
     run_experiment(M_p, gamma, eps, image_index)