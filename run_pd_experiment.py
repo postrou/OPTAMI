fixed M_p calculation

run_pd_experiment.py
@@ -40,8 +40,8 @@ def calculate_M_matrix(m):
     M_matrix = np.arange(m)
     M_matrix = cartesian_product(M_matrix, M_matrix)
     M_matrix = cdist(M_matrix, M_matrix)
-    # M_matrix /= np.max(M_matrix)
-    M_matrix /= np.median(M_matrix)
+    M_matrix /= np.max(M_matrix)
+#     M_matrix /= np.median(M_matrix)
     return torch.tensor(M_matrix, dtype=torch.double)
 
 
@@ -135,11 +135,11 @@ def calculate_lipschitz_constant(n, gamma, p_order=3, A_A_T=None, device='cpu'):
     if p_order == 3:
 #         return s.max() ** 2 * 15 / gamma ** 3
 #         return s.max() ** 2 * 15
-        return s ** 4 * 15
+        return gamma * s ** 4 * 15
     elif p_order == 1:
 #         return s.max() / gamma
 #         return s.max()
-        return s ** 2
+        return gamma * s ** 2
     else:
         raise NotImplementedError(f'Lipschitz constant calculation for p={p_order} is not implemented!')
 
@@ -180,6 +180,8 @@ def optimize(
             if i == 0:
                 init_cr_1 = cr_1
                 init_cr_2 = cr_2
+                init_phi = phi_value.item()
+                init_f = f_value.item()
             clear_output(wait=True)
             time_whole = int(time.time() - start_time)
             time_h = time_whole // 3600
@@ -189,17 +191,21 @@ def optimize(
                 f'Step #{i}',
                 f'cr_1: {init_cr_1} -> {cr_1}',
                 f'cr_2: {init_cr_2} -> {cr_2}',
+                f'phi: {init_phi} -> {phi_value.item()}',
+                f'f: {init_f} -> {f_value.item()}',
                 f'time={time_h}h, {time_m}m, {time_s}s'
             ]))
             fig, ax = plt.subplots(1, 2, figsize=(20, 8))
-            ax[0].plot(fgm_cr_1_list, label='FGM')
+            if fgm_cr_1_list is not None:
+                ax[0].plot(fgm_cr_1_list, label='FGM')
             ax[0].plot(cr_1_list, label='Tensor Method')
             ax[0].set_xlabel('iter')
             ax[0].set_ylabel('Dual gap')
             ax[0].set_yscale('log')
             ax[0].legend()
 
-            ax[1].plot(fgm_cr_2_list, label='FGM')
+            if fgm_cr_2_list is not None:
+                ax[1].plot(fgm_cr_2_list, label='FGM')
             ax[1].plot(cr_2_list, label='Tensor Method')
             ax[1].set_xlabel('iter')
             ax[1].set_ylabel('Linear constraints')