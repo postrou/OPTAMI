run_pd_experiment.py: fixed primal_var calculation

run_pd_experiment.py
@@ -58,8 +58,9 @@ def calculate_M_matrix(m):
 #     return torch.softmax(A.view(-1), dim=0)
 
 
-def calculate_x(lamb, n, M_matrix_over_gamma, ones):
-    log_X = -M_matrix_over_gamma + torch.outer(lamb[:n], ones) + torch.outer(ones, lamb[n:])
+#TODO: check
+def calculate_x(lamb, n, M_matrix_over_gamma, gamma, ones):
+    log_X = -M_matrix_over_gamma + torch.outer(lamb[:n], ones) / gamma + torch.outer(ones, lamb[n:]) / gamma
     max_log_X = log_X.max()
     log_X_stable = log_X - max_log_X
     X_stable = torch.exp(log_X_stable)
@@ -214,7 +215,7 @@ def run_experiment(M_p, gamma, eps, image_index=0, optimizer=None, max_steps=Non
             M_p=M_p,
             p_order=torch.tensor(3, device=device),
             eps=0.01,
-            calculate_primal_var=lambda lamb: calculate_x(lamb, n, M_matrix_over_gamma, ones)
+            calculate_primal_var=lambda lamb: calculate_x(lamb, n, M_matrix_over_gamma, gamma, ones)
         )
     else:
         lamb = optimizer.param_groups[0]['params'][0]